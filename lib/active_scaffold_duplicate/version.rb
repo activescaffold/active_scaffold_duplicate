0a03c4dea8f479069a7bf8a7ac074b1090156190
Sergio Cambra <sergio@enpijama.es>
2024-04-05 17:41:38 +0200

    bump to 1.2.0

--- lib/active_scaffold_duplicate/version.rb
+++ lib/active_scaffold_duplicate/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffoldDuplicate
   module Version
     MAJOR = 1
-    MINOR = 1
-    PATCH = 4
+    MINOR = 2
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
